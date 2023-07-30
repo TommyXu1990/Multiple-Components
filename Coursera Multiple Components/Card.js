5383a47e4981b25aacb642251e29269072ff6289
TommyXu1990 <67442968+TommyXu1990@users.noreply.github.com>
2023-07-30 16:14:09 +0700

    Add files via upload

---
+++ Coursera Multiple Components/Card.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Card() {
+    return (
+        <div className >
+            <h2>{props.h2}</h2>
+            <h3>{props.h3}</h3>
+        </div>
+    )
+}
+
